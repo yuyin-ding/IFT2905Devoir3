1844042dcee32adc031a105baeb26a76de85e85b
yuyin ding <you@example.com>
2021-03-17 01:23:01 -0400

    AUTHEN ET CREER UN COMPTRE 1-3 et sauter la page 1 to 2 to3

---
+++ app/src/main/java/com/example/ift2905devoir3/ui/login/createAccount2Activity.java
@@ -0,0 +1,22 @@
+package com.example.ift2905devoir3.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.ift2905devoir3.R;
+
+public class createAccount2Activity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account2);
+
+        Intent intent = getIntent();
+
+    }
+}
--- app/src/main/java/com/example/ift2905devoir3/ui/login/createAccountActivity.java
+++ app/src/main/java/com/example/ift2905devoir3/ui/login/createAccountActivity.java
@@ -3,16 +3,15 @@ package com.example.ift2905devoir3.ui.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
 
 import com.example.ift2905devoir3.R;
 
 public class createAccountActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +19,21 @@ public class createAccountActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_account);
 
         Intent intent = getIntent();
+
+        button = (Button)findViewById(R.id.goNext);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreateAccount2();
+            }
+        });
+    }
+
+    /** Called when the user taps the Suivant button */
+    public void openCreateAccount2()
+    {
+        Intent intent = new Intent(this,createAccount2Activity.class);
+        startActivity(intent);
     }
 }
